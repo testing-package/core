9a55ed3feb0ab5a3274ef21513611b85a4a55c16
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-01-01 10:39:08 +0000

    chore(base/index): update.

--- src/lib/base/index.ts
+++ src/lib/base/index.ts
@@ -1,8 +1,7 @@
-export { BooleanState } from './boolean-state.abstract';
 export { Counter } from './counter.class';
 export { DescriptionTemplate } from './description-template.class';
 export { Executable } from './executable.abstract';
-export { ExecuteState } from './execute-state.abstract';
-export { ExpectState } from './expect-state.class';
+export { ExecutionState } from './execution-state.abstract';
 export { Expect } from './expect.class';
+export { ExpectState } from './expect-state.class';
 export { Random } from './random.class';
