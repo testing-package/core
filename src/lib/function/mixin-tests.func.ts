df3c027e2f03a3ffa38b59b6301de8e9dce4a84a
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-01-01 10:39:36 +0000

    refactor(mixinTests): update.

--- src/lib/function/mixin-tests.func.ts
+++ src/lib/function/mixin-tests.func.ts
@@ -1,26 +1,32 @@
 // Class.
 import { TestingExpectation } from '../testing-expectation.class';
 // Type.
-import { Constructor } from "@angular-package/type";
-import { InstanceOfConstructor, TestingExpectations, UnionToIntersection } from "../../type";
+import { Constructor, InstanceOfConstructor, UnionToIntersection } from "@typescript-package/type";
+import { TestingExpectations } from "../../type";
 // Interface.
 import { TestingConfig } from '../../interface';
-// Mixin function: combining multiple class constructors into one
+/**
+ * @description Combining multiple class constructors into one
+ * @export
+ * @template {Constructor<any>[]} Tests
+ * @param {...Tests} tests
+ * @returns {Constructor<any>}
+ */
 export function mixinTests<Tests extends Constructor<any>[]>(...tests: Tests) {
-  return tests.reduce((acc, currTest, currIndex) => (
-    class extends acc {
+  return tests.reduce((tests, currTest, currIndex) => (
+    class extends tests {
       public expectation;
       private _expectations!: any[];
       constructor(
         execute?: boolean,
         executable?: number | number[],
-        number?: 'auto' | 'manual' | 'off',
+        index?: 'auto' | 'manual' | 'off',
         testing?: Omit<TestingConfig, 'suite'>
       ) {
         super(
           execute,
           executable,
-          number,
+          index,
           testing
         );
 
@@ -28,7 +34,7 @@ export function mixinTests<Tests extends Constructor<any>[]>(...tests: Tests) {
         const instance = new currTest(
           execute,
           executable,
-          number,
+          index,
           testing
         );
         Object.assign(this, instance);
@@ -50,7 +56,7 @@ export function mixinTests<Tests extends Constructor<any>[]>(...tests: Tests) {
               // Don't copy the constructor
               if (name !== 'constructor') {
                 Object.defineProperty(
-                  acc.prototype,
+                  tests.prototype,
                   name,
                   Object.getOwnPropertyDescriptor(currentPrototype, name) || Object.create(null)
                 );
