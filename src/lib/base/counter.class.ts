0c910c7991a0d1f9341f13184168bc1138ac1d67
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-01-01 03:04:35 +0000

    refactor(Counter): use as number identification for specs.

--- src/lib/base/counter.class.ts
+++ src/lib/base/counter.class.ts
@@ -1,85 +1,37 @@
 // Class.
-import { BooleanState } from './boolean-state.abstract';
+import { NumberState } from '@typescript-package/state';
 /**
- * @description Counter for testing purposes.
+ * @description Identification for testing purposes.
  * @export
- * @class Counter
- * @typedef {Counter}
- * @extends {BooleanState}
+ * @class NumberIdentification
+ * @extends {NumberState}
  */
-export class Counter extends BooleanState {
+export class NumberIdentification extends NumberState {
   /**
-   * @description Status of counter.
-   * @public
-   * @readonly
-   * @type {boolean}
-   */
-  public get active(): boolean {
-    return super.state;
-  }
-
-  /**
-   * @description Current counter value.
+   * @description Current identification value.
    * @public
    * @readonly
    * @type {number}
    */
   public get current(): number {
-    return this.#value;
+    return super.state;
   }
 
   /**
-   * @description
-   * @type {number}
-   */
-  #incrementSize: number;
-
-  /**
-   * @description Privately stored counter, which by default is set to `0`.
-   * @type {number}
-   */
-  #value;
-
-  /**
-   * Creates an instance of `Counter`.
+   * Creates an instance of `NumberIdentification`.
    * @constructor
-   * @param {boolean} [active=true]
-   * @param {number} [value=0]
-   * @param {number} [incrementSize=1]
    */
-  constructor(active: boolean = true, value = 0, incrementSize = 1) {
-    super(active);
-    this.#value = value;
-    this.#incrementSize = incrementSize;
+  constructor() {
+    super(0, 1);
   }
 
   /**
-   * @description Gets the actual counter.
-   * @public
-   * @returns {number}
-   */
-  public get(): number {
-    return this.#value;
-  }
-
-  /**
-   * @description Increase the counter by `1`.
-   * @public
-   * @param {number} [amount=this.#incrementSize]
-   * @returns {this} The returned value is an instance of a child class.
-   */
-  public increment(amount: number = this.#incrementSize): this {
-    this.#value += amount;
-    return this;
-  }
-  
-  /**
-   * @description Resets counter to `0`.
+   * @description Resets number identification to `0`.
    * @public
    * @returns {this} The returned value is an instance of a child class.
    */
-  public reset(): this {
-    this.#value = 0;
+  public override reset(): this {
+    super.reset();
     return this;
   }
 }
